d857b74ea96ef4c0ad7c32ada5a317f106ceaa3d
wanggang <wjiwg.liu.gg@126.com>
2020-10-30 19:35:38 +0800

    update

---
+++ ClassLoader/src/com/wanggang/j2se/MyClassLoader.java
@@ -0,0 +1,23 @@
+package com.wanggang.j2se;
+
+/**
+ * @ClassName : MyClassLoader
+ * @Description : MyClassLoader
+ * @Author : wanggang
+ * @Date: 2020-10-30 18:59
+ * @Version 1.0
+ **/
+public class MyClassLoader {
+    public static void main(String[] args) {
+        Object object = new Object();
+        System.out.print("object.getClass().getClassLoader() = ");
+        System.out.println(object.getClass().getClassLoader());
+        MyClassLoader myClassLoader = new MyClassLoader();
+        System.out.print("myClassLoader.getClass().getClassLoader() = ");
+        System.out.println(myClassLoader.getClass().getClassLoader());
+        System.out.print("myClassLoader.getClass().getClassLoader().getParent() = ");
+        System.out.println(myClassLoader.getClass().getClassLoader().getParent());
+        System.out.print("myClassLoader.getClass().getClassLoader().getParent().getParent() = ");
+        System.out.println(myClassLoader.getClass().getClassLoader().getParent().getParent());
+    }
+}

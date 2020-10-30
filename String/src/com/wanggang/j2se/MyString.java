96e01e3c3aa52827ec93054ee6f0b4c2c0edaea0
wanggang <wjiwg.liu.gg@126.com>
2020-10-30 20:05:29 +0800

    update

---
+++ String/src/com/wanggang/j2se/MyString.java
@@ -0,0 +1,16 @@
+package com.wanggang.j2se;
+
+/**
+ * @ClassName : String
+ * @Description : String
+ * @Author : wanggang
+ * @Date: 2020-10-30 19:58
+ * @Version 1.0
+ **/
+public class MyString {
+    public static void main(String[] args) {
+        String s = new String();
+        s = "Holle Word String";
+        System.out.println(s);
+    }
+}
---
+++ String/src/com/wanggang/j2se/MyStringBuffer.java
@@ -0,0 +1,16 @@
+package com.wanggang.j2se;
+
+/**
+ * @ClassName : StringBuffer
+ * @Description : StringBuffer
+ * @Author : wanggang
+ * @Date: 2020-10-30 19:57
+ * @Version 1.0
+ **/
+public class MyStringBuffer {
+    public static void main(String[] args) {
+        StringBuffer sbf = new StringBuffer();
+        sbf.append("Holle Word StringBuffer");
+        System.out.println(sbf);
+    }
+}
